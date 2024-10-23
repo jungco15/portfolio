a085bd8c81476a39fd4bc2e11eefefc947530316
omelette <jmjungcobsit1@gmail.com>
2024-10-24 00:20:02 +0800

    commit

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -1,6 +1,8 @@
 import BoxReveal from '../components/ui/box-reveal'
 import Particles from '../components/ui/particles'
 import './contact.css'
+import cv from '../../src/assets/CV.pdf'
+import { AiOutlineDownload } from "react-icons/ai";
 
 function Contact(){
     return(
@@ -19,7 +21,14 @@ function Contact(){
                     <h4>Get In Touch</h4>
                     <p>Although I’m not currently looking for any opportunities, my inbox is always  available. Still if you want to get in touch and if want to play league with me, just say hi!. I’ll try my best to get back to you! You can also try to contact me in (63+9671629307).</p>
                     </BoxReveal>
-                    </div> </div>
+                    </div> 
+                    </div>
+                    <BoxReveal>
+                    <div className='pdf py-1 flex'>
+                    <a href={cv} download="CV.pdf" target="_blank" rel="noopener noreferrer">Download my CV</a>
+                    </div>
+                    </BoxReveal>
+                    
                 
                 <BoxReveal>
                 <div className='contact-button'>
